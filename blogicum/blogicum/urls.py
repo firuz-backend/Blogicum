3c0da6ece6dd100f61dc4f3983cde8d5a210f8a3
Firuz Dadabaev <firuzdadabaev.job@gmail.com>
2025-01-16 18:37:22 +0500

    Fixed project debug-toolbar settings

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
@@ -26,3 +27,7 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+
+
+# Подключаем функцию static() к urlpatterns:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
